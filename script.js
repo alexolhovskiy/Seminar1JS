44974a01d26956620587bf0130d2b62263f572be
Sasha Sasha <sashasashaolh@gmail.com>
2024-04-03 05:32:22 +0300

    Seminar1

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello world!");
+document.getElementById("block").innerHTML = "Hello world!";
+
+let celsiusDegre = 25;
+
+let farengateDegre = (9 / 5) * celsiusDegre + 32;
+
+console.log("Celsius degres: " + celsiusDegre);
+console.log("Farengate degres: " + farengateDegre);
+
+
+let name = "Sasha";
+let admin = name;
+console.log(admin);
